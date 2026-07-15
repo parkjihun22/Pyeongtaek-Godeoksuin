0d4d9cc308de9d1553989b60f3858a0515673cdc
parkjihun22 <eyeful31@naver.com>
2026-07-16 07:27:12 +0900

    feat : 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -474,7 +474,7 @@ const Main = () => {
             {/* 입력 폼 */}
             <form
               className={styles.pcVisitForm}
-              action="https://formspree.io/f/mredqegy"
+              action="https://formspree.io/f/xjgngozq"
               method="POST"
             >
               <label htmlFor="name">
@@ -772,7 +772,7 @@ const Main = () => {
 
               <form
                 className={styles.mobileVisitForm}
-                action="https://formspree.io/f/mredqegy"
+                action="https://formspree.io/f/xjgngozq"
                 method="POST"
               >
                 <label htmlFor="name">
--- src/pages/Promotion/Customer.jsx
+++ src/pages/Promotion/Customer.jsx
@@ -32,7 +32,7 @@ export default function Customer() {
 
     try {
       // Formspree 엔드포인트로 POST
-      const res = await fetch("https://formspree.io/f/mredqegy", {
+      const res = await fetch("https://formspree.io/f/xjgngozq", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
